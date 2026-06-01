65628586cf1b8261520ba6a07bc3cccc266b1d22
Jeremy Hayes <Jeremy Hayes@Jeremy-PC.HAYES.LOCAL>
2026-06-01 17:58:30 +1200

    commit8-css2

--- modules/FlightCard.jsx
+++ modules/FlightCard.jsx
@@ -19,17 +19,10 @@ export default function FlightCard({ flight, selected, onSelect }) {
       onClick={() => onSelect(flight)}
     >
       <div>
-        <div className="flight-iata">{flight.orig}</div>
-        <div className="flight-time">{formatNZT(flight.depDate)}</div>
-      </div>
-      <div>
         <div className="flight-no">{flight.flightNo}</div>
-        <div className="flight-arrow">→</div>
-        <div className="flight-no">{flight.aircraft.split(' ')[0]}</div>
-      </div>
-      <div>
-        <div className="flight-iata">{flight.dest}</div>
-        <div className="flight-time">{formatNZT(flight.arrDate)}</div>
+        <div className="flight-iata">{flight.orig} to {flight.dest}</div>
+        <div className="flight-time">Departure: {formatNZT(flight.depDate)}</div>
+        <div className="flight-time">Arrival: {formatNZT(flight.arrDate)}</div>
       </div>
       <div className={`${seatsClass} flight-seats`}>
         {flight.available} seat{flight.available !== 1 ? 's' : ''}
