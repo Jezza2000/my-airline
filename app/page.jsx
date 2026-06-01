e2d5250c85ef34824eb7b619093505402814d079
Jeremy Hayes <Jeremy Hayes@Jeremy-PC.HAYES.LOCAL>
2026-06-01 15:38:38 +1200

    commit2

--- app/page.jsx
+++ app/page.jsx
@@ -7,14 +7,14 @@ export default function Home() {
         <p>
             A new luxury airline operating out of North Shore airport in Dairy flat north of Auckland<br/>
             We operate the following routes:<br/>
-            <ul>
-                <li>Weekend service from Dairy Flat to Sydney</li>
-                <li>Twice weekday service to Rotorua</li>
-                <li>Three times weekly to Great Barrier</li>
-                <li>Twice weekly to the Chatham Islands</li>
-                <li>Weekly service to Lake Tekapo</li>
-            </ul>
         </p>
+        <ul>
+            <li>Weekend service from Dairy Flat to Sydney</li>
+            <li>Twice weekday service to Rotorua</li>
+            <li>Three times weekly to Great Barrier</li>
+            <li>Twice weekly to the Chatham Islands</li>
+            <li>Weekly service to Lake Tekapo</li>
+        </ul>
     </div>
   );
 }
