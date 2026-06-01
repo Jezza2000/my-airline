4c1f53d4743c9458d08a6b87ee087e53b435970d
Jeremy Hayes <Jeremy Hayes@Jeremy-PC.HAYES.LOCAL>
2026-06-01 15:35:43 +1200

    commit2

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,18 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Jeremy Airlines</h1>
+        <h1>Jeremy Airlines</h1>
+        <p>
+            A new luxury airline operating out of North Shore airport in Dairy flat north of Auckland<br/>
+            We operate the following routes:<br/>
+            <ul>
+                <li>Weekend service from Dairy Flat to Sydney</li>
+                <li>Twice weekday service to Rotorua</li>
+                <li>Three times weekly to Great Barrier</li>
+                <li>Twice weekly to the Chatham Islands</li>
+                <li>Weekly service to Lake Tekapo</li>
+            </ul>
+        </p>
     </div>
   );
 }
